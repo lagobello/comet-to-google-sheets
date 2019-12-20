0f8857dceb9af8f7208806ab59f5492233bc6ea5
Vittorio Alfieri <vittorio88@gmail.com>
2019-12-20 17:09:38 -0600

    Use config files in cwd rather than path.

--- comet-to-google-sheets.py
+++ comet-to-google-sheets.py
@@ -9,17 +9,20 @@ from datetime import datetime, timedelta
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 def google_api_init():
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
 
     credentials = None
     creds = None
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(os.path.join(__location__, 'token.pickle')):
+        with open(os.path.join(__location__, 'token.pickle'), 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -27,10 +30,10 @@ def google_api_init():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                os.path.join(__location__, 'credentials.json'), SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(os.path.join(__location__, 'token.pickle'), 'wb') as token:
             pickle.dump(creds, token)
     credentials = creds
     service = discovery.build('sheets', 'v4', credentials=credentials)
